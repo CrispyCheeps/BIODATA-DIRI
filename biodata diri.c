041fe8978fae574c33895fc869b953fcc4874db0
CrispyCheeps <lsstonez6@gmail.com>
2020-09-16 12:16:03 +0700

    biodata bahasa c

---
+++ biodata diri.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	printf("\n\n");
+	printf("\t\tBIODATA DIRI\n");
+	printf("\t---------------------------\n\n");
+	printf("DATA PRIBADI:\n\n");
+	printf("Nama Lengkap\t: Ishak Febrianto\n");
+	printf("Nama Panggilan\t: Ishak / Brian\n");
+	printf("TTL\t\t: Solo, 17 Februari 2001\n");
+	printf("TB / BB	\t: 168 / 55\n");
+	printf("Pekerjaan\t: Mahasiswa\n");
+	printf("Status\t\t: Belum Kawin\n");
+	printf("email\t\t: ishakf735@gmail.com\n\n");
+	printf("*******************************************\n\n");
+	printf("RIWAYAT PENDIDIKAN:\n\n");
+	printf("SMPK Petra 5 Surabaya\t\t2013-2016\n");
+	printf("SMAK Petra 5 Surabaya\t\t2016-2019\n");
+	printf("UPNV Jatim\t\t\t2019-SOON\n");
+	printf("Jurusan\t\t\t\tTeknik Informatika\n");
+	
+	
+	return 0;
+}
